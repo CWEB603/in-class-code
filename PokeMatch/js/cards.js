a7933a168a47f5bdc712b4ae6d6e0738abf85c65
Ky <kyhomobile@gmail.com>
2024-12-16 17:43:54 -0600

    add Poke match game

---
+++ PokeMatch/js/cards.js
@@ -0,0 +1,18 @@
+const cards = [
+	'<div><img class="index186" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/186.png" /></div>', 
+	'<div><img class="index175" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/175.png" /></div>', 
+	'<div><img class="index186" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/186.png" /></div>', 
+	'<div><img class="index257" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/257.png" /></div>', 
+	'<div><img class="index103" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/103.png" /></div>', 
+	'<div><img class="index103" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/103.png" /></div>', 
+	'<div><img class="index30" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/30.png" /></div>', 
+	'<div><img class="index30" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/30.png" /></div>', 
+	'<div><img class="index175" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/175.png" /></div>', 
+	'<div><img class="index258" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/258.png" /></div>', 
+	'<div><img class="index257" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/257.png" /></div>', 
+	'<div><img class="index258" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/258.png" /></div>', 
+	'<div><img class="index130" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png" /></div>', 
+	'<div><img class="index130" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png" /></div>', 
+	'<div><img class="index84" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/84.png" /></div>', 
+	'<div><img class="index84" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/84.png" /></div>'
+];
---
+++ PokeMatch/js/finder.js
@@ -0,0 +1,145 @@
+/* Global variables */
+const pokeImgSrc = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+let counter = 0;
+const guesses = [];
+
+/**
+ * Name.		randomizeArray
+ * Summary.		This function randomizes the order of items in an arry that is passed to it.
+ * @param		{Array}		arr 		They arr that needs to be randomized
+ * @returns		{Array}		randArr		A copy of the array in a random order
+ */
+function randomizeArray(arr){
+	return arr.sort(function(){
+		return Math.random() - 0.5;
+	});
+}
+
+/**
+ * Name.		getRandPokemon
+ * Summary.		This function creates HTML image string information for random Pokemon. The random 
+ * 				images are created by sending a random number to a pokemon image repository that 
+ * 				references Pokemon by their pokedex number.
+ * @returns 	{string}	imageElement 	A string with the text necessary to create an HTML image 
+ * 												element.
+ */
+function getRandPokemon(){
+	const ranPoke = Math.ceil(Math.random()*300 + 1);
+	const divImg = document.createElement("div");
+	const pokeImg = document.createElement("img");
+	pokeImg.className = "index"+ranPoke;
+	pokeImg.src = `${pokeImgSrc}${ranPoke}.png`;
+	divImg.appendChild(pokeImg);
+	return divImg
+}
+
+/**
+ * Name.		createCards
+ * Summary.		This function will create HTML for 16 cards that have 8 pairs of prokemon and randomize the order of the cards.
+ * @returns		{Array}		randPokemon		An array of HTML script to create 16 pokemon cards.
+ */
+function createCards(){
+	const arrPokeImgs = [];
+	for(let i=0; i< 8;i++){
+		const tmpImg = getRandPokemon();
+		arrPokeImgs.push(tmpImg);
+		arrPokeImgs.push(tmpImg);
+	}
+	randomizeArray(arrPokeImgs);
+	return arrPokeImgs;
+}
+
+
+
+
+/**
+ * Name.		buildBoard
+ * Summary.		This function will take an array of cards and build a game board.
+ * @param		{Array}		cards		An array of HTML script for cards
+ */
+function buildBoard(arrImgs){
+	const htmlMain = document.querySelector("main");
+	for(let i=0; i< arrImgs.length;i++){
+		const tmpDIV = document.createElement("div");
+		htmlMain.appendChild(tmpDIV);
+	}
+	for(let i=0; i < arrImgs.length; i++){
+		htmlMain.children[i].innerHTML = arrImgs[i].innerHTML;
+	}
+}
+
+const buildCards = createCards();
+buildBoard(buildCards);
+const images = document.querySelectorAll("img");
+
+/**
+ * Name.		hideImages
+ * Summary.		Changes the opacity of images in an array to zero.
+ * @param 		{Array} 	arrOfImgesToHide 	An array of HTML image references
+ */
+function hideImages(arrCards){
+	const arrOfImagesToHide = [];
+	for(let i = 0; i< arrCards.length; i++){
+		arrCards[i].style.opacity = 0;
+	}
+}
+
+
+/**
+ * Name.		showImage
+ * Summary.		Changes the opacity of an image.
+ * @param		{image element}	img		Image element of a pokemon card we want to show.
+ */
+function showImage(paraImage){
+	paraImage.style.opacity = 1.0;
+}
+
+
+/**
+ * Name.		checkMatch
+ * Summary.		Checks if the two selected cards stored in the guesses array match and resets the 
+ * 				number of images counter to 0.
+ */
+function checkMatch(){
+	if(guesses[0].className === guesses[1].className){
+		counter = 0;
+		guesses[0].removeEventListener("click",revealCard);
+		guesses[1].removeEventListener("click",revealCard);
+	}else{
+		counter = 0;
+		setTimeout(function(){
+			hideImages(guesses);
+		},1000);
+	}	
+}
+
+
+
+
+/**
+ * Name.		revealCard
+ * Summary. 	Shows the card player slected for two guesses
+ * @param 		{event} 	e 		An event sent via an event listener
+ */
+function revealCard(){
+	counter++;
+	if(counter < 2){
+		guesses[0] = this;
+		showImage(this);
+	}else if (guesses[0].className === this.className){
+		counter--;
+	}else{
+		guesses[1] = this;
+		checkMatch();
+	}
+}
+
+setTimeout(function(){
+	hideImages(images);
+	for(let i=0; i< images.length; i++){
+		images[i].addEventListener("click",revealCard);
+	}
+},5000);
+
+/* CODE TO RUN ON PAGE LOAD */
+
